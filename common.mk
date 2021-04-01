3a49a60afaeb1bfce1b95a4dd1cf13d814ef1015
LuK1337 <priv.luk@gmail.com>
2021-04-01 13:27:49 +0200

    sm8350-common: Import sensors/2.0/multihal
    Change-Id: I2615438912974f2f0f57fd96b930d4bd11d0de5b

--- common.mk
+++ common.mk
@@ -381,7 +381,7 @@ PRODUCT_PACKAGES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@2.0-service.multihal \
+    android.hardware.sensors@2.0-service.oplus_sm8350 \
     libsensorndkbridge \
     sensors.oplus
 
