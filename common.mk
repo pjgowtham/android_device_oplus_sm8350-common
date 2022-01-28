623c0381dfddeb6431eb2a2f44a0f723ea3d87ff
pjgowtham <pjgowtham@gmail.com>
2022-01-29 04:10:39 +0530

    sm8350-common: Convert tree to oplus

--- common.mk
+++ common.mk
@@ -487,4 +487,4 @@ PRODUCT_BOOT_JARS += \
     WfdCommon
 
 # Inherit from the proprietary files makefile.
-$(call inherit-product, vendor/oneplus/sm8350-common/sm8350-common-vendor.mk)
+$(call inherit-product, vendor/oplus/sm8350-common/sm8350-common-vendor.mk)
