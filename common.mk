186a7c0d695d01c356e1ef29c1b7c6d624cb9f4d
pjgowtham <pjgowtham@gmail.com>
2022-03-12 16:51:40 +0530

    sm8350-common: Build QTI haptics hal * Override it in blob list until we reverse it.

--- common.mk
+++ common.mk
@@ -466,7 +466,7 @@ PRODUCT_COPY_FILES += \
 
 # Vibrator
 PRODUCT_PACKAGES += \
-    vendor.qti.hardware.vibrator.service.oplus
+    vendor.qti.hardware.vibrator.service
 
 PRODUCT_COPY_FILES += \
     vendor/qcom/opensource/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
