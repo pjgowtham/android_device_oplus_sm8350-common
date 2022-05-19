9d18f9b94112c7c94757273e1adbd4293451d3fd
pjgowtham <pjgowtham@gmail.com>
2022-05-19 11:13:21 +0530

    sm8350-common: Separate doze overlays for oneplus and rest of the oplus devices
    * Realme uses qti.sensor.amd for pickup events

--- common.mk
+++ common.mk
@@ -314,6 +314,14 @@ $(call inherit-product, hardware/oplus/overlay/qssi/qssi.mk)
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay-lineage
 
+ifeq ($(PRODUCT_BRAND),OnePlus)
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay-lineage-oneplus
+else
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay-lineage-oplus
+endif
+
 PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_PACKAGES += \
     CarrierConfigResCommon \
