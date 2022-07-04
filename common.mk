bda13b7a1bb4fdfa051c1cacdac13c908f6b8ddf
pjgowtham <pjgowtham@gmail.com>
2022-07-04 22:46:54 +0530

    sm8350-common: Initial changes for PE plus

--- common.mk
+++ common.mk
@@ -334,7 +334,7 @@ PRODUCT_PACKAGES += \
 $(call inherit-product, hardware/oplus/overlay/qssi/qssi.mk)
 
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-pe
 
 ifeq ($(PRODUCT_BRAND),OnePlus)
 DEVICE_PACKAGE_OVERLAYS += \
