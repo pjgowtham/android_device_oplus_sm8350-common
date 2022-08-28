5dc789db46f10be617f8dc05fb111fa1733a8ad0
pjgowtham <pjgowtham@gmail.com>
2022-08-28 19:28:51 +0530

    sm8350-common: Seperate manifest entries for snxxx NFC
    * Devices that use st21nfc don't use secure_element for NFC in stock with AIDL hal available.
    
    Co-authored-by: Sugakesshaa <83747297+bheatleyyy@users.noreply.github.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -79,6 +79,9 @@ DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     vendor/lineage/config/device_framework_matrix.xml
 DEVICE_MATRIX_FILE := $(COMMON_PATH)/compatibility_matrix.xml
 DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
+ifeq ($(TARGET_NFC_CHIPSET),snxxx)
+DEVICE_MANIFEST_FILE += $(COMMON_PATH)/manifest_snxxx.xml
+endif
 ODM_MANIFEST_FILES := $(COMMON_PATH)/manifest_odm.xml
 
 # Init
