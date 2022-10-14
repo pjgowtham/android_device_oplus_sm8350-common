8ff808eec3d5c0d361c76783b319d27f2be936dc
pjgowtham <pjgowtham@gmail.com>
2022-10-14 13:07:28 +0530

    sm8350-common: secure_element@1.2 is used by NFC and non-NFC devices
    * Referenced by QcrilNrd.so
    
    Change-Id: I4c02907bbea884c5a59ef99a94979ffc9b2613f9

--- common.mk
+++ common.mk
@@ -284,7 +284,6 @@ PRODUCT_PACKAGES += \
 # NFC
 ifdef TARGET_NFC_CHIPSET
 PRODUCT_PACKAGES += \
-    android.hardware.secure_element@1.2.vendor \
     com.android.nfc_extras \
     Tag
 ifeq ($(TARGET_NFC_CHIPSET),snxxx)
@@ -356,6 +355,7 @@ PRODUCT_PACKAGES += \
     android.hardware.radio@1.5.vendor \
     android.hardware.radio.config@1.2.vendor \
     android.hardware.radio.deprecated@1.0.vendor \
+    android.hardware.secure_element@1.2.vendor \
     libprotobuf-cpp-full \
     librmnetctl
 
