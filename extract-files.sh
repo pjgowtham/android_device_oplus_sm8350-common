19d24236276b307aa5245e047139a4d6a2b4f007
LuK1337 <priv.luk@gmail.com>
2022-11-25 15:46:37 +0100

    sm8350-common: Use libcamera_metadata_shim for unlocking cameras
    Change-Id: I985c6e317b36d8cdc6b41a9f9f625b293a8987eb

--- extract-files.sh
+++ extract-files.sh
@@ -79,10 +79,7 @@ function blob_fixup() {
             "${PATCHELF}" --replace-needed "libui.so" "libui-v30.so" "${2}"
             ;;
         vendor/lib64/hw/com.qti.chi.override.so)
-            "${SIGSCAN}" -p "45 B8 05 94" -P "1F 20 03 D5" -f "${2}" #lemonade/p
-            "${SIGSCAN}" -p "34 7D 05 94" -P "1F 20 03 D5" -f "${2}" #RMX2202            
-            "${SIGSCAN}" -p "C0 06 05 94" -P "1F 20 03 D5" -f "${2}" #RMX3360
-            "${SIGSCAN}" -p "6B 8C 05 94" -P "1F 20 03 D5" -f "${2}" #RMX3312
+            grep -q libcamera_metadata_shim.so "${2}" || "${PATCHELF}" --add-needed libcamera_metadata_shim.so "${2}"
             ;;
         vendor/lib64/vendor.qti.hardware.camera.postproc@1.0-service-impl.so)
             "${SIGSCAN}" -p "23 0B 00 94" -P "1F 20 03 D5" -f "${2}"
